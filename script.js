9ac807fdd97e0937a5d27a70213da2f9a87e13c9
mikaylak13 <mikaylaiskim@gmail.com>
2025-03-08 21:46:53 +0000

    made pics

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function fix() {
-    //Write your code here!
+    document.getElementById("coconut").remove();
+    document.getElementById("coast").remove();
+    document.getElementById("watermelon").remove();
+    document.getElementById("sunflowers").remove();
+
+    let tree = document.createElement("img");
+    tree.id = "tree";
+    tree.src = "to-add/christmasTree.jpg";
+    let photos = document.getElementById("photos");
+    photos.appendChild(tree);
+
+    let snowmen = document.createElement("img");
+    snowmen.id = "snowmen";
+    snowmen.src = "to-add/snowmen.jpg";
+    photos = document.getElementById("photos");
+    photos.appendChild(snowmen);
+
+    let stockings = document.createElement("img");
+    stockings.id = "stockings";
+    stockings.src = "to-add/stockings.jpg";
+    photos = document.getElementById("photos");
+    photos.appendChild(stockings);
 }
